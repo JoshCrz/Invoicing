c86d9abe7e1da4d54202cf28f71e65fdaf9da5b6
JoshCrossley1 <josh.crossley@sngbarratt.com>
2020-07-02 13:34:17 +0100

    added apiUrl to environment

--- app/src/app/customers/customers.component.ts
+++ app/src/app/customers/customers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomersService } from './customers.service';
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private customersService: CustomersService) { }
 
   ngOnInit(): void {
+
+    this.customersService.getCustomers().subscribe((res: any) => {
+      console.log(res);
+    })
+    
   }
 
 }
