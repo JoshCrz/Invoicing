32f418ef7dec1a065a776ef81a0e035a1582c85a
JoshCrz <35774174+JoshCrz@users.noreply.github.com>
2020-07-03 20:36:04 +0100

    additional components, lazy loaded customer components with child routes

--- app/src/app/customers/customers-routing.module.ts
+++ app/src/app/customers/customers-routing.module.ts
@@ -3,7 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CustomersComponent } from './customers.component';
 
-const routes: Routes = [{ path: '', component: CustomersComponent }];
+const routes: Routes = [
+  { 
+    path: '', component: CustomersComponent, children: [
+      { path: '', redirectTo: 'list', pathMatch: 'full'}, 
+      { path: 'list', loadChildren: () => import('./list/list.module').then(m => m.ListModule) }, 
+      { path: 'create', loadChildren: () => import('./create/create.module').then(m => m.CreateModule) },
+      { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+      { path: 'delete', loadChildren: () => import('./delete/delete.module').then(m => m.DeleteModule) }
+    ]
+  }    
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
